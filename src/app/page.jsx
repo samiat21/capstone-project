6ce9bfc660f251edc1c4f78d63039436b07d1135
samiat21 <127326107+samiat21@users.noreply.github.com>
2023-07-01 10:48:27 -0700

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -123,7 +123,7 @@ export default  function page() {
      </div>
 
      <div className='flex justify-center mt-[50px]'>
-      <h2 className='text-[40px] text-[#1d2951] w-[500px] font-bold'>Our Top Doctors' List With Categories</h2>
+      <h2 className='text-[40px] text-[#1d2951] w-[500px] font-bold'>Our Top Doctors List With Categories</h2>
      </div>
 
      <div className="flex ml-[160px] w-[250px]  border-b-2 mt-3">
